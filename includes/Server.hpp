b96f78c45eac5c6856a93099842e4b520d543fbc
Martim Carneiro Pacheco Nunes Pinto <mcarneir@c2r3s4.42porto.com>
2024-08-13 14:37:30 +0100

    first connection

---
+++ includes/Server.hpp
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Server.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcarneir <mcarneir@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/13 12:06:14 by mcarneir          #+#    #+#             */
+/*   Updated: 2024/08/13 14:21:32 by mcarneir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SERVER_HPP
+#define SERVER_HPP
+
+#include "webserver.hpp"
+
+class Server
+{
+	public:
+			Server(std::string ip, int port);
+			~Server();
+			void startListen();
+			void acceptConnection(int &new_socket);
+			
+	
+	private:
+			std::string m_ip_address;
+			int	m_socket;
+			int m_port;
+			int m_new_socket;
+			//long m_incomingMessage;
+			struct sockaddr_in m_socketAddress;
+			unsigned int m_socketAddress_len;
+			std::string m_serverMessage;
+
+			
+			int startServer();
+			void closeServer();
+			std::string buildResponse();
+			void sendResponse();
+	
+};
+
+#endif
--- includes/webserver.hpp
+++ includes/webserver.hpp
@@ -6,7 +6,7 @@
 /*   By: mcarneir <mcarneir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/12 14:56:53 by mcarneir          #+#    #+#             */
-/*   Updated: 2024/08/12 16:33:22 by mcarneir         ###   ########.fr       */
+/*   Updated: 2024/08/13 14:26:21 by mcarneir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,23 @@
 #include <string>
 #include <poll.h>
 #include <sys/socket.h>
+#include <arpa/inet.h>
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sstream>
 #include <string.h>
+#include "Server.hpp"
 
 #define PORT 8080
+#define BUFFER_SIZE 30720
+
+
+void log(const std::string &message);
+void exitError(const std::string &error);
+
 
 
 
